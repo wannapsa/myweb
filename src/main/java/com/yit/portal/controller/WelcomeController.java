3a84f63b951869f6d5fd6499d013924722b7bbdc
Kriangsak Pornpantiva <kriangsak.po@yipintsoi.com>
2020-08-14 09:52:27 +0700

    update test rollout 5

--- src/main/java/com/yit/portal/controller/WelcomeController.java
+++ src/main/java/com/yit/portal/controller/WelcomeController.java
@@ -31,7 +31,7 @@ public class WelcomeController {
 	@RequestMapping("/dd")
 	public String welcome(Map<String, Object> model) {
 		model.put("message", this.message);
-		return "welcome test rollout 4";
+		return "welcome test rollout 5";
 	}
 
 	@RequestMapping( value = "/", method = RequestMethod.GET)
